ae0044839c9dfbed67dd2195e1747d2a8417b872
fernandog301 <fernandog301g@gmail.com>
2024-01-12 16:03:23 -0800

    test

---
+++ Migrations/20240113000156_migrationtwo.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GutierrezFStudentDatabaseProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class migrationtwo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+Fernando Gutierrez
+1/12/2024
+Desc: Creating 3 endpoints (Create, Read, Delete), Implement SQLite database to store and manage data,create endpoints for the following CRUD operations. 
+
